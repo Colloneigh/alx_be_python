96b28958fc555eacdbe65107f7d62f32e1664533
Daniel Kinyanjui Mbagara <mbagaradaniel@gmail.com>
2024-09-21 16:41:41 +0300

    task 0 Basic Arithmetic Exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+number1 = 10
+number2 = 5
+
+#addition
+result_addition = number1 + number2
+print(f"Addition of {number1} and {number2} is {result_addition}")
+
+#subtraction
+result_subtraction = number1 - number2
+print(f"Subtraction of {number1} and {number2} is {result_subtraction}")
+
+#product
+result_product = number1 * number2
+print(f"Product of {number1} and {number2} is {result_product}")
