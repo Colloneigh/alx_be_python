68e5f40f341f6d35da7a5104da0ca7c95d2b9fa6
Daniel Kinyanjui Mbagara <mbagaradaniel@gmail.com>
2024-09-22 20:32:07 +0300

    task 3 Drawing Patterns with Nested Loops

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,9 @@
+#prompt user to enert a positive integer
+pattern_size = int(input("Enter the size of the pattern:"))
+
+row = 0
+while row < pattern_size:
+    for _ in range(pattern_size):
+        print("*", end = "")
+    print() 
+    row += 1
